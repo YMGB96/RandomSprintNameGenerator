f7b667d15dcf1d240410784ae3659bee1d95db28
YMGB96 <yannick.brands@gmx.net>
2022-12-15 13:51:22 +0100

    Initial Commit

---
+++ RandomSprintNameGenerator(SwiftVersion)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RandomSprintNameGenerator(SwiftVersion)
+//
+//  Created by Yannick Brands on 15.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomSprintNameGenerator(SwiftVersion)/RandomSprintNameGenerator_SwiftVersion_App.swift
@@ -0,0 +1,17 @@
+//
+//  RandomSprintNameGenerator_SwiftVersion_App.swift
+//  RandomSprintNameGenerator(SwiftVersion)
+//
+//  Created by Yannick Brands on 15.12.22.
+//
+
+import SwiftUI
+
+@main
+struct RandomSprintNameGenerator_SwiftVersion_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
