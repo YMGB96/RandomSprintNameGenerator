eda7f82a53d86393a3ade5cf7f6ceb067f69540c
YMGB96 <yannick.brands@gmx.net>
2023-01-12 13:38:29 +0100

    added minimal design to votingResults, added CoreData entity for Sprintnames

--- RandomSprintNameGenerator(SwiftVersion)/Views/VoteResultView.swift
+++ RandomSprintNameGenerator(SwiftVersion)/Views/VoteResultView.swift
@@ -18,31 +18,44 @@ struct VoteResultView: View {
     var body: some View {
         if (voteHasOneWinner) {
             VStack{
-                Text("The chosen Sprintname is \(chosenSprintName)!\n(in the future, you can save the result and there will actually be something resembling desing")
+                Text("The chosen Sprintname is:")
+                    .font(.title2)
+                Text("\(chosenSprintName)")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.green)
+                Text("\n")
                 Button(action: {
                     showingVotingResult = false
                 }, label: {
-                    Text("Dismiss")
+                    Text("Save & Exit")
+                        .font(.title2)
                 })
+                Text("saving to be added soon")
+                    .font(.caption)
+                    .foregroundColor(.gray)
             }
         }
         if (!voteHasOneWinner) {
             VStack {
                 Text("The Vote has been tied.")
+                    .font(.title2)
+                Text("")
                 Button(action: {
                     randomWordFetcher.prepForTiebreaker(topVoteCount: topVoteCount)
                     castedVotes = 0
                     dismiss()
                 }, label: {
-                    Text("Re-Vote")
+                    Text("Tiebreaker vote")
+                        .font(.title2)
                 })
                 
             }
         }
     }
 }
-//struct VoteResultView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        VoteResultView(randomWordFetcher: RandomWordFetcher(), chosenSprintName:"Testwinner", voteHasOneWinner: true, topVoteCount: 4)
-//    }
-//}
+struct VoteResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        VoteResultView(randomWordFetcher: RandomWordFetcher(), chosenSprintName: "Testwinner", showingVotingResult: .constant(true), castedVotes: .constant(4), voteHasOneWinner: false, topVoteCount: 3)
+    }
+}
